fcc37c81c4b99689f6182783c763c6c3265be5f7
YanivBohbot <Yanivbohbot5@gmail.com>
2025-02-14 18:01:37 +0200

    modified the api to send data to kafka

--- ai-data-analytics-platform/app/api/ingest.py
+++ ai-data-analytics-platform/app/api/ingest.py
@@ -1,12 +1,18 @@
 from fastapi import APIRouter , UploadFile, File
-
 import pandas as pd 
 import io 
+from app.services.kafka_producer import send_message
+
 
 router = APIRouter()  
 
 @router.post("/upload")
-async def uploadFile(file: UploadFile = File(...)):
+async def upload_dataset(file: UploadFile = File(...)):
     contents = await file.read()
     df = pd.read_csv(io.BytesIO(contents))
-    return {"filesmname": file.filename,  "columns": list(df.columns)}
+    data = {"filesmname": file.filename,  "columns": list(df.columns)}
+    
+    send_message(data)
+    
+    return {"message": "File uploaded successfully"}    
+    
